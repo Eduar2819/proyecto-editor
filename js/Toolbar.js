446f303f34ae75234fca759a2fb125ac2387f2bc
Eduardo <eduardo.sosa2819@gmail.com>
2020-12-29 01:23:34 -0600

    Avance de los elementos principales

---
+++ js/Toolbar.js
@@ -0,0 +1,34 @@
+import { UIPanel, UIButton } from '../libs/ui.js';
+
+function Toolbar () {
+    var container = new UIPanel();
+    container.setId('toobar');
+
+    // translate / rotate /scale 
+    var translateIcon = document.createElement('img');
+    //translateIcon.title = string.getKey('toolbar/translate');
+    translateIcon.src = 'images/translate.svg';
+
+    var translate = new UIButton();
+    translate.dom.className = 'Button selected';
+    translate.dom.appendChild(translateIcon);
+    translate.onClick(function (){
+
+        //signal.trasformModeChanged.dispatch('translate');
+    });
+    container.add(translate);
+
+    var rotateIcon = document.createElement('img');
+    //rotateIcon.title = string.getKey('toolbar/rotate');
+    rotateIcon.src = 'images/rotate.svg';
+
+    var rotate = new UIButton();
+    rotate.dom.appendChild(rotateIcon);
+    rotate.onClick( function (){
+        //signals.transformModeChanged.dispatch('rotate');
+    });
+    container.add(rotate);
+
+    return container;
+}
+export {Toolbar};
