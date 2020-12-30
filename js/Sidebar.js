67f262c519da730c532dc37bd69db9156325fd36
Eduardo <eduardo.sosa2819@gmail.com>
2020-12-30 16:45:23 -0600

    iniciando el sidebar

---
+++ js/Sidebar.js
@@ -0,0 +1,12 @@
+import {UIPanel, UIButton} from '../libs/ui.js';
+
+function Sidebar (){
+
+    var container = new UIPanel();
+    container.setId('sidebar');
+
+
+    return container;
+
+} 
+    export {Sidebar};
