75bf39527d6751a3781a3eee198f91ce4fab443f
Eduardo <eduardo.sosa2819@gmail.com>
2020-12-31 01:23:28 -0600

    agregando del header

---
+++ js/Menubar.js
@@ -0,0 +1,50 @@
+import {UIPanel, UIButton} from '../libs/ui.js';
+
+function Menubar(){
+
+    var container = new UIPanel();
+    container.setId('menubar');
+
+    // Botones del menu para el editor (texto, modelo, imagen, ...)
+
+    var ModeloIcon = document.createElement('img');
+    ModeloIcon.src = 'images/modelo.svg'
+
+    var ModeloForm = document.createElement('form');
+    ModeloForm.style.display = 'none';
+    document.body.appendChild(ModeloForm);
+
+    var ModeloInput = document.createElement('input');
+    ModeloInput.multiple = true;
+    ModeloInput.type = 'file';
+
+    var Modelo = new UIButton();
+    Modelo.dom.appendChild(ModeloIcon);
+    Modelo.onClick( function(){
+        ModeloInput.click();
+    });
+    container.add(Modelo);
+
+    // TEXTO
+
+    var VideoIcon = document.createElement('img');
+    VideoIcon.src = 'images/video.svg'
+
+    var VideoForm = document.createElement('form');
+    VideoForm.style.display = 'none';
+    document.body.appendChild(VideoForm);
+
+    var VideoInput = document.createElement('input');
+    VideoInput.multiple = true;
+    VideoInput.type = 'file';
+
+    var Video = new UIButton();
+    Video.dom.appendChild(VideoIcon);
+    Video.onClick( function(){
+        VideoInput.click();
+    });
+    container.add(Video);
+    return container;
+
+}
+    export {Menubar};
