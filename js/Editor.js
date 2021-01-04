57c3ba15cf5fdf997296e8b72c3945c08095133e
Eduardo <eduardo.sosa2819@gmail.com>
2021-01-04 13:11:42 -0600

    agregando la cuadricula guia

--- js/Editor.js
+++ js/Editor.js
@@ -6,8 +6,25 @@ Camera_principal.position.set(0, 5, 10);
 Camera_principal.lookAt( new THREE.Vector3() );
 
 function Editor (){
+    var Signal = signals.Signal;
+
+    this.signals = {
+        windowResize: new Signal(),
+
+    };
 
     this.camera = Camera_principal.clone();
     this.scene = new THREE.Scene();
     this.scene.name = 'Scene';
+    this.scene.background = new THREE.Color( 0xFFFFFF );
+
+    this.object = {};
+
+    this.materialRefCounter = new Map();
+
+    this.animations = {};
+    this.mixer = new THREE.AnimationMixer (this.scene);
+
+    this.selected = {};
 }
+    export {Editor};
--- js/View.js
+++ js/View.js
@@ -1,12 +1,73 @@
 import * as THREE from '/node_modules/three/build/three.module.js';
 import {UIPanel} from '../libs/ui.js';
 
-function View(){
+
+function View(editor){
+
+    var signals = editor.signals;
+    
+
+    var clock = new THREE.Clock();
+    
+    var camera = editor.camera;
+    var scene = editor.scene;
+    var mixer = editor.mixer;
+
+    var objects = [];
+
     var container = new UIPanel();
     container.setId('view');
     container.setPosition('absolute');
 
+    var renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( container.dom.offsetWidth, container.dom.offsetHeight );
+    container.dom.appendChild( renderer.domElement );
+
     var grid = new THREE.GridHelper( 30, 30, 0x444444, 0x888888 );
+    grid.material.transparent = true;
+    scene.add(grid);
+
+    
+    
+    function animate (){
+        
+        var delta = clock.getDelta();
+        requestAnimationFrame(animate);
+
+        var needsUpdate = false;
+
+        if (mixer.stats.action.inUse > 0){
+            mixer.update(delta);
+            needsUpdate = true;
+        }
+
+        if(needsUpdate === true) render();
+    }
+
+    function updateAspectRatio() {
+
+		camera.aspect = container.dom.offsetWidth / container.dom.offsetHeight;
+		camera.updateProjectionMatrix();
+
+	}
+
+    function render(){
+       
+        renderer.render(scene, camera);
+
+    }
+    // Signals
+	signals.windowResize.add( function () {
+
+		updateAspectRatio();
+
+		renderer.setSize( container.dom.offsetWidth, container.dom.offsetHeight );
+
+		render();
+
+	} );
+    
 
     return container;
 }
